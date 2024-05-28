cdb8e7bd3973291a365cf51a9766b4750696a798
broy9501 <64872666+broy9501@users.noreply.github.com>
2024-05-28 11:59:41 +0100

    Create Q2 Add Two Numbers.py

---
+++ answers/Q2 Add Two Numbers.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        head = tail = ListNode(0)
+        
+        while l1 is not None or l2 is not None or carry != 0:
+            val1 = l1.val if l1 is not None else 0
+            val2 = l2.val if l2 is not None else 0
+
+            sum = val1 + val2 + carry
+            number = sum % 10
+            carry = sum // 10
+
+            newNode = ListNode(number)
+            tail.next = newNode
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        result = head.next
+        head.next = None
+        return result
